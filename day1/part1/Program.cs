5560a39ccff512e01da37d91eee7f17e2ac24f09
David Lacasse <david.lacasse1337@gmail.com>
2023-12-08 14:47:05 -0500

    1.1

---
+++ day1/part1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+string[] lines = await File.ReadAllLinesAsync("data");
+int sum = 0;
+foreach (string line in lines)
+{
+    int? firstDigit = null;
+    int? lastDigit = null;
+    for (int i = 0; i < line.Length; ++i)
+    {
+        var currentChar = line[i];
+        if (Char.IsDigit(currentChar))
+        {
+
+            int value = int.Parse(currentChar.ToString());
+            if (firstDigit is null)
+            {
+                firstDigit = value;
+            }
+
+            lastDigit = value;
+        }
+    }
+    if (firstDigit.HasValue && lastDigit.HasValue)
+    {
+        sum += (firstDigit.Value * 10) + lastDigit.Value;
+    }
+}
+Console.WriteLine(sum);
